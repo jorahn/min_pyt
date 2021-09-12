e4b374cfa0ce961ccf059bddd20bdc3c6db68b59
Jonathan Rahn <rahn.jonathan@gmail.com>
2021-09-12 13:37:36 +0200

    Update __init__.py

--- minpt/__init__.py
+++ minpt/__init__.py
@@ -86,20 +86,20 @@ def test(dataloader, model, loss_fn, device):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-def run():
-    # Get cpu or gpu device for training.
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+def run(batch_size=64, learning_rate=1e-3, epochs=5, model=None, device=None):
+    if not device:
+        # Get cpu or gpu device for training.
+        device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using {} device".format(device))
 
     train_data, test_data = get_fashion_mnist()
-    batch_size = 64
     train_dl, test_dl = get_dl(train_data, test_data, batch_size)
-    model = get_model(device)
+    
+    if not model:
+        model = get_model(device)
     loss_fn = nn.CrossEntropyLoss()
-    learning_rate = 1e-3
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    epochs = 5
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train(train_dl, model, loss_fn, optimizer, device)
