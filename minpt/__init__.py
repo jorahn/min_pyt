5d9825343e00d86fc53b8edf2f4e6069aa999281
Jonathan Rahn <rahn.jonathan@gmail.com>
2021-09-12 19:37:59 +0200

    Update __init__.py

--- minpt/__init__.py
+++ minpt/__init__.py
@@ -85,7 +85,7 @@ def test(dataloader, model, loss_fn, device):
     test_loss /= num_batches
     correct /= size
     #print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    return loss, correct
+    return test_loss, correct
 
 def run(batch_size=64, learning_rate=1e-3, epochs=5, model=None, device=None):
     if not device:
